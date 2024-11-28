1d5c78085d68492e922d9218040a7e3905842507
Mat <71132805+mat512@users.noreply.github.com>
2024-11-28 15:38:01 +0100

    feat: add job card

--- src/app/features/jobs/jobs.component.ts
+++ src/app/features/jobs/jobs.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-jobs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './jobs.component.html',
-  styleUrls: ['./jobs.component.scss']
+  styleUrls: ['./jobs.component.scss'],
 })
-export class JobsComponent {
-
-}
+export class JobsComponent {}
