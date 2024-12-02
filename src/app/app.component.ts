26c2d420d8b86cc8a036719c1e02118da189f614
Mat <71132805+mat512@users.noreply.github.com>
2024-12-02 23:05:08 +0100

    feat: add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./shared/components/navigation/navigation.component";
 import { HeaderComponent } from "./shared/components/header/header.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
