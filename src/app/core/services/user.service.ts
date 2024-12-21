c21dddb4290c25800ed674ccbf66d74e5a630e15
pviprey <viprey.pierre@gmail.com>
2024-12-21 14:35:58 +0100

    feat: adding the user data inside local storage

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -10,13 +10,21 @@ export class UserService {
   private currentUserSubject: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
   public currentUser$ = this.currentUserSubject.asObservable();
 
+  constructor() {
+    const user = localStorage.getItem('user');
+    if(user != null){
+      this.currentUserSubject.next(JSON.parse(user));
+    }
+  }
 
   login(user: User): void {
     this.currentUserSubject.next(user);
+    localStorage.setItem('user', JSON.stringify(user));
   }
 
   logout(): void {
     this.currentUserSubject.next(null);
+    localStorage.removeItem('user');
   }
 
   get currentUser(): User | null {
